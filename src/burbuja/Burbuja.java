3dfdb13721cd9205c8c001acec4ea33b7ccaafbd
Moisés Breton <moisesbreton11@gmail.com>
2022-10-31 22:36:58 -0600

    Si jala

---
+++ src/burbuja/Burbuja.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package burbuja;
+
+/**
+ *
+ * @author Berke
+ */
+public class Burbuja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+       int []arreglo = {9,3,};
+        ordenador o = new ordenador ();
+        o.main(arreglo);
+        
+        for(int i =0;i<arreglo.length; i++)
+        {
+            System.out.println(arreglo[1]);
+        }
+
+    }
+}
